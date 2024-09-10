d1d380bb4ede8589a51b997ea03c46105f461aad
Abdi Awale <abdi.awale5@gmail.com>
2024-09-10 18:10:05 +0000

    Install a production-ready webserver for Heroku,Add gunicorn==20.1.0 to the requirements.txt file,Create a file named Procfile and declare web process in it and and replace DEBUG=True with DEBUG=False.

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=$x!q)hm6601#a&=qyu&)*3w1(u5xf!vo0yd6q_ud10gv-pn(_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [ '8000-cawaale256-djangoblog-62vbmf9apuo.ws.codeinstitute-ide.net' ]
+ALLOWED_HOSTS = [ '8000-cawaale256-djangoblog-62vbmf9apuo.ws.codeinstitute-ide.net','.herokuapp.com' ]
 
 
 # Application definition
