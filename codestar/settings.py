8f471e5946f09600b768e77394007cc126135096
Abdi Awale <abdi.awale5@gmail.com>
2024-09-12 19:46:29 +0000

    Building the homepage with Django generic views

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=$x!q)hm6601#a&=qyu&)*3w1(u5xf!vo0yd6q_ud10gv-pn(_'
 # SECRET_KEY ='b494LqgCOEdsztotq4aTEJtmoVmlRhar'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [ '8000-cawaale256-djangoblog-62vbmf9apuo.ws.codeinstitute-ide.net','.herokuapp.com', 
     '8000-cawaale256-djangoblog-62vbmf9apuo.ws.codeinstitute-ide.net/' ]
